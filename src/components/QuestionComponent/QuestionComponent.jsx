4cae64099cc707563d135ce826cd60abab0999b3
Guttz <gustavopguto@gmail.com>
2019-04-12 13:23:42 -0300

    Adding layout breakpoints to enhance responsiveness

--- src/components/QuestionComponent/QuestionComponent.jsx
+++ src/components/QuestionComponent/QuestionComponent.jsx
@@ -5,6 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import '../../style.css';
+import './QuestionComponent.css'
 import AlternativeButtons from '../helpers/ButtonComponent/AlternativeButtonComponent'
 import Grid from "@material-ui/core/Grid";
 import {connect} from 'react-redux'
@@ -26,29 +27,9 @@ class QuestionComponent extends React.Component {
         return (
             <React.Fragment>
                 <Grid container direction="column" spacing={5}>
-                    <Grid item>
-                        <CssBaseline/>
 
-                        <Grid container>
-                            <Grid item xs={4}>
-                            </Grid>
-                            <Grid item xs={4}>
-                                <img className='responsive-img' alt="explanatory"
-                                     src="https://cdn4.buysellads.net/uu/1/41312/1545083324-1539370929-mailchimp-Yellow-260x200.png"/>
-                            </Grid>
-                            <Grid item xs={4}>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-
-                    <Grid item xs={5}>
-                    </Grid>
-
-                    <Grid item xs={5}>
+                    <Grid item >
                         <h1> {this.props.currentQuestion.title} - {this.props.currentQuestion.category} </h1>
-                    </Grid>
-
-                    <Grid item xs={5}>
                     </Grid>
 
                     <Grid item>
@@ -59,26 +40,21 @@ class QuestionComponent extends React.Component {
                     </Grid>
 
                     <Grid item>
-                        <Grid container spacing={3}>
-                            <Grid item xs={4}>
-                            </Grid>
-                            <Grid item xs={4}>
+                        <Grid container spacing={3} direction="row" justify="center" alignItems="center">
+                            <Grid item xs={10} md={6} lg={4} >
                                 <Slider color="primary" {...this.settings}>
                                     <div>
                                         <img className='responsive-img' alt="explanatory"
                                              src="https://cdn4.buysellads.net/uu/1/41312/1545083324-1539370929-mailchimp-Yellow-260x200.png"/>
                                     </div>
-                                    <div>
-                                        <img className='responsive-img' alt="explanatory"
-                                             src="https://cdn4.buysellads.net/uu/1/41312/1545083324-1539370929-mailchimp-Yellow-260x200.png"/>
+                                    <div className='responsive-img'>
+                                        <iframe width="100%" height="100%" className='responsive-img' src="https://www.youtube.com/embed/u_Nneiwgres" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                     </div>
                                     <div>
                                         <img className='responsive-img' alt="explanatory"
                                              src="https://cdn4.buysellads.net/uu/1/41312/1545083324-1539370929-mailchimp-Yellow-260x200.png"/>
                                     </div>
                                 </Slider>
-                            </Grid>
-                            <Grid item xs={4}>
                             </Grid>
                         </Grid>
                     </Grid>
