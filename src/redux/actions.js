57fbb4093227a47a0ad4ecbccff27db82b0a2f6c
Gustavo Morais <gustavopguto@gmail.com>
2019-11-12 18:54:26 -0300

    Adding actions template file

---
+++ src/redux/actions.js
@@ -0,0 +1,37 @@
+/*
+ * action types
+ */
+
+export const ADD_TODO = '2'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+/*
+ * other constants
+ */
+
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+/*
+ * action creators
+ */
+
+export function setCurrentQuestion(text) {
+  return { type: ADD_TODO, text }
+}
+
+export function addTodo(text) {
+  return { type: ADD_TODO, text }
+}
+
+export function toggleTodo(index) {
+  return { type: TOGGLE_TODO, index }
+}
+
+export function setVisibilityFilter(filter) {
+  return { type: SET_VISIBILITY_FILTER, filter }
+}
