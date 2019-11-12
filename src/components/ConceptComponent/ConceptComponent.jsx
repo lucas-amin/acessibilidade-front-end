2f4fd755d5af3c68bede069f144535e03eb77a35
Gustavo Morais <gustavopguto@gmail.com>
2019-11-12 18:54:44 -0300

    Basic working ConceptComponent with redux-connect

--- src/components/ConceptComponent/ConceptComponent.jsx
+++ src/components/ConceptComponent/ConceptComponent.jsx
@@ -3,24 +3,41 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-export class ConceptComponent extends React.Component {
-    constructor(concept) {
+import { connect } from 'react-redux'
+import store from '../../redux/store';
+
+import * as action from './../../redux/actions'
+
+class ConceptComponent extends React.Component {
+    constructor() {
         super();
-        this.state = concept;
     }
 
+    componentWillMount(){
+        debugger
+        this.props.dispatch(action.setCurrentQuestion())    
+    }
     render() {
         return (
             <React.Fragment>
                 <CssBaseline/>
-                <h1> Concept X: {this.state.title}</h1>
+                <h1> Concept X: {this.props.title}</h1>
                 <Container maxWidth="sm">
                     <Typography component="div" style={{backgroundColor: '#cfe8fc', height: '30vh'}}>
-                        Concept text: {this.state.text}
+                        Concept text: {this.props.text}
                     </Typography>
                 </Container>
             </React.Fragment>
         )
     }
 }
+
+function mapStateToProps (store) {
+    return({
+        title: store.currentConcept.title,
+        text: store.currentConcept.text
+    })
+}
+
+export default connect(mapStateToProps)(ConceptComponent)
 
