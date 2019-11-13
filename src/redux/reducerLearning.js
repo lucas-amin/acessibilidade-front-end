5f98dd1f9e02101f56dd4bf999038a9a4285ed5d
Gustavo Morais <gustavopguto@gmail.com>
2019-11-12 21:26:09 -0300

    Adding real example to reducer

--- src/redux/reducerLearning.js
+++ src/redux/reducerLearning.js
@@ -1,24 +1,31 @@
 const defaultState = {
-  title: "Question 12",
-  category: "Mechanics",
-  text: "What is a inclusive mechanic?"
-};
+  "title": "Narrativa",
+  "media": "link",
+  "category": "Narrativa",
+  "intro": "Apresenta histórias e experiências que possam representar a diversidade que existe no mundo, de modo que os jogadores não se sintam excluídos ou estereotipados.",
+  "full_explanation": "Apresenta histórias e experiências que possam representar a diversidade que existe no mundo, de modo que os jogadores não se sintam excluídos ou estereotipados. \
+  \
+  Exemplos de jogos que seguem esse pricipio:\
+  \
+  1) Mass Effect\
+  Em Mass Effect o personagem Jeff “Joker” Moreau tem um caso moderado a grave da síndrome de Vrolik (Osteogenesis imperfecta), que causa extrema fragilidade nos ossos; ele nasceu com fraturas graves nas pernas e, mesmo com a medicina moderna, acha quase impossível andar, contando com muletas e aparelhos para as pernas.\
+  \
+  2) Grand Theft Auto V\
+  Em GTA 5 o personagem Lester Crest sofre de uma doença devastadora que gradualmente desgastou suas habilidades motoras durante sua vida e o deixou em cadeira de rodas. Devido à sua mobilidade limitada, Lester cresceu consideravelmente acima do peso e também sofre de asma."
+}
 
 export const reducerLearning = function(state = defaultState, action) {
-  let currentConcept = {
-    title: "Question 1",
-    category: "Mechanics",
-    text: "What is a inclusive mechanic?"
-  };
-    
-  if (action.type == "INC") {
-    console.log("inc");
-    currentConcept.title = "Question 2";
-  } else if (action.type == "DEC") {
-    currentConcept.title = "Question 3";
-    console.log("DEC");
-  } else {
-    currentConcept.title = "Question 4";
+  let currentConcept = {};
+
+  if (action.type == "UPDATE_CONCEPT") {
+    currentConcept.title = action.payload.title;
+    currentConcept.media = action.payload.media;
+    currentConcept.category = action.payload.category;
+    currentConcept.intro = action.intro;
+    currentConcept.full_explanation = action.full_explanation;
+
+    return currentConcept;
   }
-  return currentConcept;
+
+  return defaultState;
 };
