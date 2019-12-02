f03cf270bb553b3b99e60c3e1ac809bb286b81f9
Gustavo Morais <gustavopguto@gmail.com>
2019-12-02 18:15:35 -0300

    Removing unused code

--- src/redux/reducerQuestions.js
+++
@@ -1,3 +0,0 @@
-export const reducerQuestions = function(state=[], action){
-  return state;
-}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import {reducerQuestion} from './reducerQuestion'
 import {reducerLearning} from './reducerLearning'
-import {reducerQuestions} from './reducerQuestions'
 
 import {combineReducers, createStore} from 'redux'
 
 const reducers = combineReducers({  
   currentQuestion: reducerQuestion,
-  currentConcept: reducerLearning,
-  questions: reducerQuestions
+  currentConcept: reducerLearning
 })
 
 const store = createStore(reducers)
@@ -16,7 +14,7 @@ store.subscribe( () => {
   console.log('state\n', store.getState());
   var test = store.getState()
   
-  debugger;
+  //debugger;
 });
 
 export default store
